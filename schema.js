1749594d66a33a812cd917462023e7a35ef024e4
Teppala Sagarika <sagarikateppala@gmail.com>
2025-10-02 16:05:56 +0530

    Fixed Joi schema and the form to create new listing

--- schema.js
+++ schema.js
@@ -4,12 +4,12 @@ const Joi = require('joi');
 const listingSchema = Joi.object({
     listing: Joi.object({
         title: Joi.string().required(),
-        description: Joi.string.required(),
-        location: Joi.string.required(),
-        country: Joi.string.required(),
+        description: Joi.string().required(),
+        location: Joi.string().required(),
+        country: Joi.string().required(),
         price: Joi.number().required().min(0),
         image: Joi.string().allow("", null),
     }).required()
-})
+});
 
-module.exports(listingSchema);
+module.exports = { listingSchema };
