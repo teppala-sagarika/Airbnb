b5a2a147809337c961e98fc9b769dad6d5a5dd51
Teppala Sagarika <sagarikateppala@gmail.com>
2026-01-01 12:26:27 +0530

    Added preventOwnerBooking middleware

--- middleware.js
+++ middleware.js
@@ -58,3 +58,20 @@ module.exports.isReviewAuthor = async(req, res, next) => {
     }
     next();
 };
+
+module.exports.preventOwnerBooking = async(req, res, next) => {
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
+
+    if (!listing) {
+        req.flash("error", "Listing not found");
+        return res.redirect("/listings");
+    }
+
+    if (listing.owner.equals(res.locals.currUser._id)) {
+        req.flash("error", "You cannot book your own listing");
+        return res.redirect(`/listings/${id}`);
+    }
+
+    next();
+};
--- routes/payment.js
+++ routes/payment.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utilities/wrapAsync.js");
 const paymentController = require("../controllers/payment.js");
-const { isLoggedIn } = require("../middleware.js");
+const { isLoggedIn, preventOwnerBooking } = require("../middleware.js");
 
 //initiate the payment process for a specific listing
-router.post("/:id/checkout", isLoggedIn, wrapAsync(paymentController.createPaymentIntent));
+router.post("/:id/checkout", isLoggedIn, preventOwnerBooking, wrapAsync(paymentController.createPaymentIntent));
 
 // redirects to after successful payment
-router.get("/:id/booked", isLoggedIn, wrapAsync(paymentController.confirmBooking));
+router.get("/:id/booked", isLoggedIn, preventOwnerBooking, wrapAsync(paymentController.confirmBooking));
 
 module.exports = router;
